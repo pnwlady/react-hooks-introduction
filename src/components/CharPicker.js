09cfc18938bf1e2d74cb8dba24653e073de887fa
Heidi Laursen <heidilaursen@yahoo.com>
2019-10-08 10:23:29 -0700

    converted from class based component to stateful functional component with useState and useEffect

--- src/components/CharPicker.js
+++ src/components/CharPicker.js
@@ -1,12 +1,15 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './CharPicker.css';
 
-class CharPicker extends Component {
-  state = { characters: [], isLoading: false };
+const CharPicker = props => {
+  const  [ loadedCharacters, setLoadedCharacters ] = useState([]);
+  const [ isLoading, setIsLoading ] = useState(false);
+  // state = { characters: [], isLoading: false };
 
-  componentDidMount() {
-    this.setState({ isLoading: true });
+
+  useEffect( () => {
+    setIsLoading( true );
     fetch('https://swapi.co/api/people')
       .then(response => {
         if (!response.ok) {
@@ -16,34 +19,34 @@ class CharPicker extends Component {
       })
       .then(charData => {
         const selectedCharacters = charData.results.slice(0, 5);
-        this.setState({
-          characters: selectedCharacters.map((char, index) => ({
+        setIsLoading(false);
+        setLoadedCharacters(
+          selectedCharacters.map((char, index) => ({
             name: char.name,
             id: index + 1
-          })),
-          isLoading: false
-        });
+          }))
+         );
       })
       .catch(err => {
         console.log(err);
+        setIsLoading(false);
       });
-  }
+  }, []);
 
-  render() {
     let content = <p>Loading characters...</p>;
 
     if (
-      !this.state.isLoading &&
-      this.state.characters &&
-      this.state.characters.length > 0
+      !isLoading &&
+      loadedCharacters &&
+      loadedCharacters.length > 0
     ) {
       content = (
         <select
-          onChange={this.props.onCharSelect}
-          value={this.props.selectedChar}
-          className={this.props.side}
+          onChange={props.onCharSelect}
+          value={props.selectedChar}
+          className={props.side}
         >
-          {this.state.characters.map(char => (
+          {loadedCharacters.map(char => (
             <option key={char.id} value={char.id}>
               {char.name}
             </option>
@@ -51,13 +54,12 @@ class CharPicker extends Component {
         </select>
       );
     } else if (
-      !this.state.isLoading &&
-      (!this.state.characters || this.state.characters.length === 0)
+      !isLoading &&
+      (!loadedCharacters || loadedCharacters.length === 0)
     ) {
       content = <p>Could not fetch any data.</p>;
     }
     return content;
-  }
 }
 
 export default CharPicker;
